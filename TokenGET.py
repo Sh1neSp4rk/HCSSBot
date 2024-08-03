5a669c404b3ccfc1bbb380e5eb6082e31a26f8eb
Sh1neSp4rk <31965386+Sh1neSp4rk@users.noreply.github.com>
2024-08-03 06:03:56 +0000

    tokenget

--- TokenGET.py
+++ TokenGET.py
@@ -1,14 +1,18 @@
 import requests
+import os
 
 url = "https://api.hcssapps.com/identity/connect/token"
 
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+grant_type = os.getenv('GRANT_TYPE')
+scope = os.getenv('SCOPE')
+
 payload = {
-  "client_id": "string",
-  "client_secret": "string",
-  "grant_type": "string",
-  "scope": "string",
-  "code": "string",
-  "redirect_uri": "string"
+  "client_id": client_id,
+  "client_secret": client_secret,
+  "grant_type": grant_type,
+  "scope": scope,
 }
 
 headers = {"Content-Type": "application/x-www-form-urlencoded"}

f7cd890e14518cd79b215aef1c727b8fafdf26e1
Sh1neSp4rk <31965386+Sh1neSp4rk@users.noreply.github.com>
2024-09-26 09:44:29 -0400

    Update install_browsers.sh

--- install_browsers.sh
+++ install_browsers.sh
@@ -4,21 +4,27 @@
 echo "Updating package list..."
 sudo apt-get update
 
-# Install Google Chrome
-echo "Installing Google Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb
+# Install Google Chrome from Chrome for Testing
+echo "Fetching the latest stable Chrome for Testing version..."
+CHROME_URL=$(curl -sS "https://googlechromelabs.github.io/chrome-for-testing/latest-versions-per-milestone-with-downloads.json" | jq -r '.["115"].downloads.chrome[].url' | grep "linux64" | head -n 1)
+
+# Download and install Chrome
+echo "Downloading Chrome from $CHROME_URL"
+wget $CHROME_URL -O google-chrome-stable_linux64.deb
+sudo apt-get install -y ./google-chrome-stable_linux64.deb
 
 # Clean up
-rm google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_linux64.deb
 
 # Get the version of Chrome installed
-CHROME_VERSION=$(google-chrome --version | grep -oP '[\d.]+')
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+\.\d+' | head -1 | cut -d '.' -f 1-3)
 
-# Install ChromeDriver that matches the Chrome version
-echo "Installing ChromeDriver..."
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
-wget https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+# Install ChromeDriver that matches the Chrome version from CfT
+echo "Fetching the latest ChromeDriver for version $CHROME_VERSION"
+CHROME_DRIVER_URL=$(curl -sS "https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build-with-downloads.json" | jq -r --arg version "$CHROME_VERSION" '.[$version].downloads.chromedriver[].url' | head -1)
+
+echo "Downloading ChromeDriver from $CHROME_DRIVER_URL"
+wget $CHROME_DRIVER_URL -O chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip -d /usr/local/bin
 rm chromedriver_linux64.zip
 
